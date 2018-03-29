b0c3b7be486840ab74939e86e3313aecf517cf09
Jehy <fate@jehy.ru>
2018-03-29 18:22:41 +0300

    fix some linting

--- migrations/20180322160230_init.js
+++ migrations/20180322160230_init.js
@@ -1,33 +1,5 @@
-/*
-*
-    type: logEntry._type,
-    name: logEntry._source.fields.name,
-    timestamp: logEntry._source['@timestamp'],
-    level: logEntry._source.fields.type,
-    message: message.trim(),
-    msgName: messageName.trim(),
-    msgId: logEntry._source['msgId'],
-    env: logEntry._source['chef_environment'],
-    host: logEntry._source['host'],
-    role: logEntry._source['role'],
-
-    ,
-  kibanaErrors       {
-  kibanaErrors          "type": "twapi-avia",
-  kibanaErrors          "name": "ADDITIONAL_SERVICES",
-  kibanaErrors          "timestamp": "2018-03-22T12:53:35.707Z",
-  kibanaErrors          "level": "E",
-  kibanaErrors          "message": "_ex_createAdditionalServicesOrders_0 BAD SERVICE 1 undefined order e8ff45b4-7d2b-4aa3-87f1-603716c304b4",
-  kibanaErrors          "msgName": "_ex_createAdditionalServicesOrders_0",
-  kibanaErrors          "msgId": "6623620001b5defcf9a487bbc0bb441d",
-  kibanaErrors          "env": "production-b",
-  kibanaErrors          "host": "order-production-b-1i5ro",
-  kibanaErrors          "role": "api-order"
-  kibanaErrors       },
-
-    */
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('logs', function(t) {
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('logs', (t) => {
     t.increments('id').unsigned().primary();
     t.dateTime('eventDate').notNull().index();
     t.string('name').notNull().index();
@@ -39,10 +11,10 @@ exports.up = function(knex, Promise) {
     t.string('host').notNull();
     t.string('role').notNull().index();
     t.text('message').nullable();
-    t.enum('level', ['I', 'E', 'W','Z','D','']).notNull().index();
+    t.enum('level', ['I', 'E', 'W', 'Z', 'D', '']).notNull().index();
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = (knex, Promise) => {
   return knex.schema.dropTableIfExists('logs');
 };
--- migrations/20180323144932_fix_date.js
+++ migrations/20180323144932_fix_date.js
@@ -1,12 +1,11 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.alterTable('logs', function(t) {
+exports.up = (knex, Promise) => {
+  return knex.schema.alterTable('logs', (t) => {
     t.specificType('eventDate', 'DATETIME(6)').alter();
   });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.alterTable('logs', function(t) {
+exports.down = (knex, Promise) => {
+  return knex.schema.alterTable('logs', (t) => {
     t.date('eventDate').notNull().index().alter();
   });
 };

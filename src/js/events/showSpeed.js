03958fd2647926ed39899de93bc8bcacc1ba25f7
Jehy <fate@jehy.ru>
2019-04-03 15:51:57 +0300

    better display speed, bump deps

--- src/js/events/showSpeed.js
+++ src/js/events/showSpeed.js
@@ -75,7 +75,7 @@ function addGraph(graph, data)
       renderTo: graph[0],
       events: {
         load: (chart) => {
-          setTimeout(() => chart.target.reflow());
+          setTimeout(() => chart.target.reflow(), 1000);
         },
       },
     },
@@ -115,20 +115,24 @@ function showSpeed(data) {
   }
   // //
   // eventDate, name,type,msgId,env,host,role,message
-  const notNeededFields = ['message', 'name', 'index', 'type', 'guid', 'msgId'];
+  const notNeededFields = ['message', 'name', 'index', 'type', 'guid', 'msgId', 'id'];
   const needFields = Object.keys(data[0])
     .filter(key => !notNeededFields.includes(key));
   const header = data.msgName;
   const thead = $('<thead>');
   const headerTds = needFields.map(key => `<th>${key}</th>`);
   thead.append(headerTds);
-  const table = $('<table class="table"/>');
-  table.append(thead);
-  const tbody = $('<tbody>');
   let needStripe = true;
-  data.forEach((log) => {
+  const container = $('<div/>');
+  data.forEach((log, index) => {
     const graph = $('<div/>');
     addGraph(graph, log.message);
+    const table = $('<table class="table"/>');
+    if (index === 0)
+    {
+      table.append(thead);
+    }
+    const tbody = $('<tbody>');
     needStripe = !needStripe;
     let trStyle = '';
     if (needStripe) {
@@ -136,26 +140,20 @@ function showSpeed(data) {
     }
     log.eventDate = moment(log.eventDate).format('HH:mm:ss');
     const meta = needFields.map(key => `<td>${log[key]}</td>`).join('');
-    const errMessage = JSON.stringify(log.message, null, 3);
-    // tr.append(Object.values(err).map((val => `<td>${val}</td>`)).join(''));
+    const logMessage = $('<div style="display: none"/>')
+      .html(`<pre>${JSON.stringify(log.message, null, 3)}</pre>`);
     tbody.append(`<tr${trStyle}>${meta}</tr>`);
-    const tdGraph = $(`<td colspan="${needFields.length}"></td>`).append(graph);
+    table.append(tbody);
     const showBtn = $('<button type="button" class="btn btn-default">Show data</button>');
-    const btnTd = $(`<td colspan="${needFields.length}"></td>`).append(showBtn);
-    const btnTr = $('<tr/>').append(btnTd);
-    tbody.append($('<tr/>').append(tdGraph));
-    tbody.append(btnTr);
-    const message = $(`<td colspan="${needFields.length}" class="err-msg" style="display: none">`).html(`<pre>${errMessage}</pre>`);
+    const message = $(`<td colspan="${needFields.length}" class="err-msg" style="display: none">`).html(`<pre>${logMessage}</pre>`);
     tbody.append($(`<tr${trStyle}>`).append(message));
     showBtn.click(()=>{
-      message.toggle();
+      logMessage.toggle();
     });
+    container.append(table).append(graph).append(showBtn);
   });
-  table.append(tbody);
 
 
-  const container = $('<div/>');
-  container.append(table);
   Utils.showModal(header, container);
 }
 

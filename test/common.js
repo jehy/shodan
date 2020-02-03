686050b321fc4447ac2beb91807be7b50fba24e4
Jehy <fate@jehy.ru>
2020-02-03 16:47:41 +0300

    update tests

--- test/common.js
+++ test/common.js
@@ -11,22 +11,25 @@ describe('common front and back libs', () => {
       const link = commonLib.makeKibanaLink('twapi-avia', 'SEARCH_MONITORING', 'monitorDirectFlights', 'https://logs.twiket.com');
       assert.equal(link, 'https://logs.twiket.com/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),'
         + 'time:(from:now-1h,mode:quick,to:now))&_a=(columns:!(message),filters:!((\'$state\':(store:appState),'
-        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,'
-        + 'params:(query:monitorDirectFlights,type:phrase),type:phrase,value:monitorDirectFlights),'
-        + 'query:(match:(msgName:(query:monitorDirectFlights,type:phrase)))),'
-        + '(\'$state\':(store:appState),meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:fields.name,negate:!f,'
+        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,params:(query:monitorDirectFlights,type:phrase),'
+        + 'type:phrase,value:monitorDirectFlights),query:(match:(msgName:(query:monitorDirectFlights,type:phrase)))),'
+        + '(\'$state\':(store:appState),meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,'
         + 'params:(query:SEARCH_MONITORING,type:phrase),type:phrase,value:SEARCH_MONITORING),'
-        + 'query:(match:(fields.name:(query:SEARCH_MONITORING,type:phrase))))),'
-        + 'index:\'twapi-avia-*\',interval:auto,query:(language:lucene,query:\'\'),sort:!(\'@timestamp\',desc))');
+        + 'query:(match:(fields.name:(query:SEARCH_MONITORING,type:phrase)))),(\'$state\':(store:appState),'
+        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,params:(query:E,type:phrase),'
+        + 'type:phrase,value:E),query:(match:(fields.type:(query:E,type:phrase))))),index:\'twapi-avia-*\',interval:auto,'
+        + 'query:(language:lucene,query:\'\'),sort:!(\'@timestamp\',desc))');
     });
     it('should make links when msgName is auto', () => {
 
       const link = commonLib.makeKibanaLink('twapi-avia', 'SEARCH_MONITORING', 'AUTO monitorDirectFlights', 'https://logs.twiket.com');
       assert.equal(link, 'https://logs.twiket.com/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),'
         + 'time:(from:now-1h,mode:quick,to:now))&_a=(columns:!(message),filters:!((\'$state\':(store:appState),'
-        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:fields.name,negate:!f,'
+        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,'
         + 'params:(query:SEARCH_MONITORING,type:phrase),type:phrase,value:SEARCH_MONITORING),'
-        + 'query:(match:(fields.name:(query:SEARCH_MONITORING,type:phrase))))),index:\'twapi-avia-*\','
+        + 'query:(match:(fields.name:(query:SEARCH_MONITORING,type:phrase)))),(\'$state\':(store:appState),'
+        + 'meta:(alias:!n,disabled:!f,index:\'twapi-avia-*\',key:msgName,negate:!f,params:(query:E,type:phrase),'
+        + 'type:phrase,value:E),query:(match:(fields.type:(query:E,type:phrase))))),index:\'twapi-avia-*\','
         + 'interval:auto,query:(language:lucene,query:\'\'),sort:!(\'@timestamp\',desc))');
     });
   });
